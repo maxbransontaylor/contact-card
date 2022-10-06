dc77b45976e42225e18328902f4488080dd5356d
Max Taylor <maxbransontaylor@gmail.com>
2022-10-06 17:26:39 -0400

    aw crud

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,3 +13,81 @@ export async function initDb() {
     },
   });
 }
+export async function getDb() {
+  // Create a connection to the IndexedDB database and the version we want to use.
+  const contactDb = await openDB("contact_db", 1);
+  // Create a new transaction and specify the store and data privileges.
+  const tx = contactDb.transaction("contacts", "readonly");
+  // Open up the desired object store.
+  const store = tx.objectStore("contacts");
+
+  const request = store.getAll();
+
+  const result = await request;
+  console.log("result.value", result);
+  return result;
+}
+
+export async function postDb(name, email, phone, profile) {
+  const contactDb = await openDB("contact_db", 1);
+  const tx = contactDb.transaction("contacts", "readwrite");
+
+  const store = tx.objectStore("contacts");
+
+  const request = store.add({
+    name: name,
+    email: email,
+    phone: phone,
+    profile: profile,
+  });
+  const result = await request;
+  console.log("data saved to db", result);
+}
+export async function deleteDb(id) {
+  const contactDb = await openDB("contact_db", 1);
+
+  const tx = contactDb.transaction("contacts", "readwrite");
+
+  const store = tx.objectStore("contacts");
+  const request = store.delete(id);
+  const result = await request;
+  console.log("result.value", result);
+  return result?.value;
+}
+export const editDb = async (id, name, email, phone, profile) => {
+  console.log("PUT to the database");
+
+  const contactDb = await openDB("contact_db", 1);
+
+  const tx = contactDb.transaction("contacts", "readwrite");
+
+  const store = tx.objectStore("contacts");
+
+  const request = store.put({
+    id: id,
+    name: name,
+    email: email,
+    phone: phone,
+    profile: profile,
+  });
+  const result = await request;
+  console.log("🚀 - data saved to the database", result);
+};
+window.editCard = (e) => {
+  // Grabs the id from the button element attached to the contact card and sets a global variable that will be used in the form element.
+  let profileId = parseInt(e.dataset.id);
+
+  // Grabs information to pre-populate edit form
+  let editName = e.dataset.name;
+  let editEmail = e.dataset.email;
+  let editPhone = e.dataset.phone;
+
+  document.getElementById("name").value = editName;
+  document.getElementById("email").value = editEmail;
+  document.getElementById("phone").value = editPhone;
+
+  form.style.display = "block";
+
+  // Toggles the Submit button so that it now Updates an existing contact instead of posting a new one
+  submitBtnToUpdate = true;
+};
